5493389bfd7a9f5d0e69aaaaa8d74e7466477539
DagurasuSK <dagurasucontacts@gmail.com>
2024-12-16 16:55:48 -0300

    Trail effect

---
+++ scripts/trail.gd
@@ -0,0 +1,17 @@
+extends Line2D
+@onready var player: CharacterBody2D = $"../.."
+
+const MAX_POINTS :int = 30
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	add_point(get_parent().global_position)
+	if points.size() > MAX_POINTS:
+		remove_point(0)
+	disable_trail()
+
+
+func disable_trail():
+	if not player.is_on_wall() and not player.is_on_floor():
+		self.visible = false
+	else:
+		self.visible = true
