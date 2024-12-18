0c0d457171ca6be65a67a1560756e189c4424189
DagurasuSK <dagurasucontacts@gmail.com>
2024-12-18 01:01:12 -0300

    fixes code

--- stage/levels/start_adventure.gd
+++ stage/levels/start_adventure.gd
@@ -1,6 +1,10 @@
 extends Area2D
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
 	Hud.play_stopwatch()
 	MusicManager.play_gameplay_music()
 	StageManager.go_to_next_stage()
